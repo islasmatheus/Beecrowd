d5babea540f29cd6c68cb0c2b734c8e190c4a936
islasmatheus <devislasmatheus@gmail.com>
2024-08-21 14:59:29 -0300

    Atualizado

---
+++ Java/Área do Círculo/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double raio;
+        double n = 3.14159;
+        raio = sc.nextDouble();
+        double area = area(raio, n);
+        System.out.format("A=%.4f\n", area);
+        sc.close();
+    }
+
+    public static double area(double raio, double n) {
+        return  3.14159*(raio*raio);
+    }
+
+}
